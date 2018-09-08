b5c37fcf78a54d4772abb6b339b2234925b953ac
Ndelforno <nicolas.delforno@gmail.com>
2018-09-08 12:44:52 -0400

    Lowest and highest done

--- lowest_and_highest.rb
+++ lowest_and_highest.rb
@@ -1,6 +1,6 @@
 i=0
 array_die = []
-until i == 5
+until i == 10
 die = Random.rand(1..6)
 array_die << die
 array_die = array_die.sort
@@ -9,5 +9,3 @@ end
 array_die.each do |num|
   puts "The result of your roll is #{num}"
 end
-puts "The lowest number is #{array_die.first}"
-puts "The highest number is #{array_die.last}"
