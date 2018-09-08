ca9d85b273d914dd21bccb9131707c3c8c52e44d
Ndelforno <nicolas.delforno@gmail.com>
2018-09-08 13:31:24 -0400

    permutations

---
+++ permutations.rb
@@ -0,0 +1,17 @@
+array_die = [1,2,3,4,5,6]
+
+def permutation (array)
+i = 1
+array_perm = []
+  until i == 7
+    array.each do |num|
+      array_perm << [i,num]
+      end
+      i += 1
+    end
+    return array_perm
+end
+
+puts array_perm = permutation(array_die)
+
+# puts array_perm [1]
