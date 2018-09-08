303ab21ba3c9e86281267c138df8172017cb113a
Ndelforno <nicolas.delforno@gmail.com>
2018-09-08 12:51:43 -0400

    dice done

---
+++ dice.rb
@@ -0,0 +1,5 @@
+die1 = Random.rand(1..6)
+die2 = Random.rand(1..6)
+total = die1 + die2
+puts "You rolled #{die1} and #{die2}"
+puts "Your total is #{total}"
--- lowest_and_highest.rb
+++ lowest_and_highest.rb
@@ -1,6 +1,6 @@
 i=0
 array_die = []
-until i == 10
+until i == 5
 die = Random.rand(1..6)
 array_die << die
 array_die = array_die.sort
@@ -9,3 +9,5 @@ end
 array_die.each do |num|
   puts "The result of your roll is #{num}"
 end
+puts "The lowest number is #{array_die.first}"
+puts "The highest number is #{array_die.last}"
